ee280e6477915a882ee678e651adfcb847dc21cd
kamalesh0703 <kamaleshponmani0703@gmail.com>
2023-11-10 14:03:47 +0530

    main section grid layout change

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function carttoggle() {
     var x = document.getElementById("mob-cart-box");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
+    if (x.style.display === "block") {
       x.style.display = "none";
+    } else {
+      x.style.display = "block";
     }
   }
 
